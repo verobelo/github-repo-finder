658b90727acc9d14c970b5b397579aefe73b4702
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-16 19:51:10 +0200

    Import Header, Footer & Search components

--- one-repo-away/src/App.jsx
+++ one-repo-away/src/App.jsx
@@ -1,7 +1,15 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-  return <Header />;
+  return (
+    <div className="wrapper">
+      <Header />
+      <SearchBar />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
