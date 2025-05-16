944171063bb332cc755d212abf174de5c5e34a12
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-16 19:50:40 +0200

    Create static search input

---
+++ one-repo-away/src/components/SearchBar.jsx
@@ -0,0 +1,23 @@
+export default function SearchBar() {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Search repositories..." />
+      <button className="search-bar__button">
+        <svg
+          width={26}
+          height={26}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
+            stroke="#808080"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
