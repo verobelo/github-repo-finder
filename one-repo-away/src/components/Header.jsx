3a106cc3b38c1f564d76a73977209a265c1b15db
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-16 15:56:17 +0200

    Add Header component

---
+++ one-repo-away/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>One Repo Away.</h1>
+      <p>
+        <em>The perfect GitHub repo is just one search away.</em>
+      </p>
+    </header>
+  );
+}
