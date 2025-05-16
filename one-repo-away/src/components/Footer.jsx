81ddce9db43ef2a5c4147c3c169b358a77bc4ecd
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-16 18:40:59 +0200

    Add Footer component

---
+++ one-repo-away/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p> &copy; 2025 Veronika Belozerova. All rights reserved.</p>
+      <p>
+        <a
+          href="https://www.flaticon.com/free-icons/magnifying-glass"
+          title="magnifying glass icons">
+          Magnifying glass icon created by chehuna - Flaticon
+        </a>
+      </p>
+    </footer>
+  );
+}
