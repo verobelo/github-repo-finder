84cf50c92aae465d5be722a70b0588e6f31cc0f8
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-22 19:46:42 +0200

    Add padding

--- one-repo-away/src/components/Footer.jsx
+++ one-repo-away/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Container, Text } from "@chakra-ui/react";
 
 export default function Footer() {
   return (
-    <Container as="footer" centerContent>
+    <Container as="footer" centerContent pt="4" pb="4">
       <Text> &copy; 2025 Veronika Belozerova. All rights reserved.</Text>
     </Container>
   );
