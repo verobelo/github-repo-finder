fa0de65588d73d82d34db15ca3c1674c8be0c9c2
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-22 18:44:05 +0200

    Add Container

--- one-repo-away/src/components/Header.jsx
+++ one-repo-away/src/components/Header.jsx
@@ -1,14 +1,14 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Container, Heading, Text } from "@chakra-ui/react";
 
 export default function Header() {
   return (
-    <header style={{ textAlign: "center" }}>
+    <Container as="header" style={{ textAlign: "center" }}>
       <Heading as="h1" size="3xl">
         One Repo Away.
       </Heading>
       <Text fontStyle="italic" color="gray.700">
         The perfect GitHub repo is just one search away.
       </Text>
-    </header>
+    </Container>
   );
 }
