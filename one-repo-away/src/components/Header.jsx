875cd911bd8f36836ead8a62f06a652b1bc131d6
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-22 23:14:19 +0200

    Change font family for heading

--- one-repo-away/src/components/Header.jsx
+++ one-repo-away/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Container, Heading, Text } from "@chakra-ui/react";
 export default function Header() {
   return (
     <Container as="header" centerContent pt="4" pb="4">
-      <Heading as="h1" size="3xl">
+      <Heading as="h1" size="3xl" fontFamily="fira_codelight">
         One Repo Away.
       </Heading>
       <Text fontStyle="italic" color="gray.700">
