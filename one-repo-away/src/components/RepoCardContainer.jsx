6c26a402fd9526b66c73f81687144c6aaffdead8
verobelo <142001535+verobelo@users.noreply.github.com>
2025-05-22 20:44:52 +0200

    Add pagination

--- one-repo-away/src/components/RepoCardContainer.jsx
+++ one-repo-away/src/components/RepoCardContainer.jsx
@@ -1,15 +1,47 @@
-import { Grid } from "@chakra-ui/react";
+import { Grid, Pagination, ButtonGroup, IconButton } from "@chakra-ui/react";
+import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
 import RepoCard from "./RepoCard";
 
 export default function RepoCardContainer() {
   return (
-    <Grid templateColumns="repeat(2, 1fr)" gap={4}>
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-    </Grid>
+    <>
+      <Grid templateColumns="repeat(2, 1fr)" gap={4} justifyItems="center">
+        <RepoCard />
+        <RepoCard />
+        <RepoCard />
+        <RepoCard />
+        <RepoCard />
+        <RepoCard />
+      </Grid>
+
+      <Pagination.Root
+        count={20}
+        pageSize={2}
+        defaultPage={1}
+        ml="auto"
+        mr="auto">
+        <ButtonGroup variant="ghost" size="sm">
+          <Pagination.PrevTrigger asChild>
+            <IconButton>
+              <LuChevronLeft />
+            </IconButton>
+          </Pagination.PrevTrigger>
+
+          <Pagination.Items
+            render={(page) => (
+              <IconButton variant={{ base: "ghost", _selected: "outline" }}>
+                {page.value}
+              </IconButton>
+            )}
+          />
+
+          <Pagination.NextTrigger asChild>
+            <IconButton>
+              <LuChevronRight />
+            </IconButton>
+          </Pagination.NextTrigger>
+        </ButtonGroup>
+      </Pagination.Root>
+    </>
   );
 }
